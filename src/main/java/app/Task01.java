eaacba4fd1495425308f6802ef48dbc34d7e2cb4
Russ_r2d2 <rustam.kazymzhanov21@gmail.com>
2025-09-21 19:02:51 -0400

    task 02

--- src/main/java/app/Task01.java
+++ src/main/java/app/Task01.java
@@ -3,6 +3,6 @@ package app;
 public class Task01 {
     public static void main(String[] args) {
 
-        System.out.println("Код задачи 1");
+        System.out.println("Код задачи 1-");
     }
 }
---
+++ src/main/java/app/Task02.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Task02 {
+    public static void main(String[] args) {
+
+        System.out.println("Код задачи 2");
+    }
+}
